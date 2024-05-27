342e13e7eba2f4261cbd03d7f85f7188d4b594e1
gaon12 <34927797+gaon12@users.noreply.github.com>
2024-05-27 19:51:30 +0900

    코드 개선

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,33 +1,18 @@
- function checkanswers()
- {var val ="Dokdo";
- if(val == (document.getElementById("print1").value))
- 
-  {var val ="Zalophus Japonicus";
-  if(val == (document.getElementById("print2").value))
- 
-   {var val ="1905";
-   if(val == (document.getElementById("print3").value))
- 
-    {var val ="Liancourt Rocks"
-    if(val == (document.getElementById("print4").value))
-        
-        {var val ="Methane Hydrate"
-        if(val == (document.getElementById("print5").value))
- 
-            {alert("Wow! Perfect! You've studied a lot about Dokdo.");
-            window.open('../wow/index.html');
-            }
-                else{alert("Goal is in front of you!");}
-                }
- 
-                    else{alert("Let's do a little more!");}
-                    }
- 
-                        else{alert("Just try harder.");}
-                            }
- 
-        	                    else{alert("We'll study again!");}
-                                }
-                                
-                                    else{alert("Let's do it again after you cool off. You have to work really hard.");}
-    	                            }
+function checkAnswers() {
+    var answers = ["Dokdo", "Zalophus Japonicus", "1905", "Liancourt Rocks", "Methane Hydrate"];
+    var correctCount = 0;
+
+    for (var i = 0; i < answers.length; i++) {
+        if (document.getElementById("print" + (i + 1)).value === answers[i]) {
+            correctCount++;
+        } else {
+            alert("Starting with the " + (i + 1) + "th question, there is an incorrect answer.");
+            return;
+        }
+    }
+
+    if (correctCount === 5) {
+        alert("Wow! Perfect! You've studied a lot about Dokdo.");
+        window.open('../wow/index.html');
+    }
+}
